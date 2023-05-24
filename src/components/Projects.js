517c493c7761c69516539c5c36d30a033a4cbe4d
ankit <07.kankit@gmail.com>
2023-05-24 19:20:31 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import newsletter from "../assets/img/newsletter.png";
 import todolist from "../assets/img/todolist.png";
-import calculator from "../assets/img/calculator.png";
+import exercise from "../assets/img/exercise.png";
 
 export const Projects = () => {
   const projects = [
@@ -14,6 +14,14 @@ export const Projects = () => {
       link: "https://silly-onesies-mite.cyclic.app/",
     },
     {
+      title: "Exercise Db",
+      description:
+        "A gym exercises web app built using Node.js, React, and Material UI, integrating Rapid API for fetching exercise details and displaying relevant YouTube videos.",
+      imageUrl: exercise,
+      // sourceCode:'',
+      link: "https://ok-gym.netlify.app",
+    },
+    {
       title: "ToDoList",
       description:
         "Full stack todolist made using Javascript Node ejs and Mongodb.",
@@ -21,13 +29,13 @@ export const Projects = () => {
       // sourceCode:'',
       link: "https://famous-beret-toad.cyclic.app",
     },
-    {
-      title: "Calculator",
-      description: "Made using vanila javascript.",
-      imageUrl: calculator,
-      sourceCode: "https://github.com/k-ankit07/simpleCalculator",
-      link: "https://k-ankit07.github.io/simpleCalculator/",
-    },
+//     {
+//       title: "Calculator",
+//       description: "Made using vanila javascript.",
+//       imageUrl: calculator,
+//       sourceCode: "https://github.com/k-ankit07/simpleCalculator",
+//       link: "https://k-ankit07.github.io/simpleCalculator/",
+//     },
   ];
 
   return (
